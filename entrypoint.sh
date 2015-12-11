b8411e4b3f4c4042a83aba62b9263039952346c5
sebastien.stinkeste <sebastien.stinkeste@alterway.fr>
2015-12-11 15:22:07 +0100

    update

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+#
+# set localtime
+ln -sf /usr/share/zoneinfo/${LOCALTIME:-Europe/Paris} /etc/localtime
+
+#
+# functions
+
+function replace_vars() {
+  eval "cat <<EOF
+  $(<$1)
+EOF
+  " > $1
+}
+
+replace_vars '/etc/nginx/nginx.conf'
+
+# Run
+if [[ ! -z "$1" ]]; then
+    exec ${*}
+else
+    exec nginx -g 'daemon off;'
+fi
