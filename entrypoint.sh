9f67623d734bd7802bf9923b57930da7bccfcec4
sebastien.stinkeste <sebastien.stinkeste@alterway.fr>
2015-12-21 18:34:57 +0100

    try

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,14 @@ ln -sf /usr/share/zoneinfo/${LOCALTIME:-Europe/Paris} /etc/localtime
 #
 # functions
 
-function set_conf {
-    echo ''>$2; IFSO=$IFS; IFS=$(echo -en "\n\b")
-    for c in `printenv|grep $1`; do echo "`echo $c|cut -d "=" -f1|awk -F"$1" '{print $2}'` $3 `echo $c|cut -d "=" -f2`" >> $2; done;
-    IFS=$IFSO
+function replace_vars() {
+  eval "cat <<EOF
+  $(<$1)
+EOF
+  " > $1
 }
 
-set_conf '/etc/nginx/default.conf' 
+replace_vars '/etc/nginx/default.conf'
 
 # Run
 if [[ ! -z "$1" ]]; then
